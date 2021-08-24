3e01f365a2f47420b32540621c9fddfa84df7597
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-24 15:51:28 -0300

    add dashboard component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Dashboard } from "../src/pages/Dashboard";
 
 const index = () => {
-  return <div>olá mundo</div>;
+  return <Dashboard />;
 };
 
 export default index;
