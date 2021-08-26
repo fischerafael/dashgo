76926ec80f3e66091c44b24b29e7223772c7350d
Rafael Fischer <rafaelsanfischer@gmail.com>
2021-08-26 15:12:36 -0300

    started the finish of dashbar

--- src/components/organisms/Sidebar.tsx
+++ src/components/organisms/Sidebar.tsx
@@ -1,10 +1,42 @@
-import { Box, VStack } from '@chakra-ui/react'
+import { Box, VStack, Text, Link, Icon } from '@chakra-ui/react'
+import { HiOutlineRefresh, HiOutlineTrendingUp } from 'react-icons/hi'
 
 export const Sidebar = () => {
     return (
-        <Box as="aside" w="64" bg="gray.300">
-            <VStack spacing="12" align="flex-start">
-                oi
+        <Box as="aside" w="64" py="8">
+            <VStack spacing="10" align="flex-start">
+                <Box w="full">
+                    <Text
+                        fontWeight="bold"
+                        fontSize="small"
+                        letterSpacing="widest"
+                    >
+                        INCOME
+                    </Text>
+                    <VStack spacing="4" py="4" align="flex-start">
+                        <Link display="flex" w="full" color="blue.500">
+                            <Icon as={HiOutlineTrendingUp} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">
+                                Single
+                            </Text>
+                        </Link>
+                        <Link display="flex" w="full" color="blue.500">
+                            <Icon as={HiOutlineRefresh} fontSize="20" />
+                            <Text ml="4" fontWeight="medium">
+                                Recurring
+                            </Text>
+                        </Link>
+                    </VStack>
+                </Box>
+                <Box>
+                    <Text
+                        fontWeight="bold"
+                        fontSize="small"
+                        letterSpacing="widest"
+                    >
+                        COMMUNITY
+                    </Text>
+                </Box>
             </VStack>
         </Box>
     )
